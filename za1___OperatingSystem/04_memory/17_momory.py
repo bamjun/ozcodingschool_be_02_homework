525b514bc8184fd61e735c200c97c427e573411f
bamjun <khy5116@naver.com>
2024-04-08 13:41:13 +0900

    os_memory

---
+++ za1___OperatingSystem/04_memory/17_momory.py
@@ -0,0 +1,12 @@
+# 문자열 객체를 my_anme이 참조했다.
+
+my_name = "bamjun"
+
+your_name = my_name
+
+my_name = 1
+your_name = 2
+
+# 레퍼런스 카운트가 0이 되었다.
+# 소멸 대상
+# 가비지 컬렉터로 제거
---
+++ za1___OperatingSystem/04_memory/18_memory.js
@@ -0,0 +1,15 @@
+let players = {
+    boy : {
+        Bergkamp : "Striker"
+    }
+}
+
+let persons = players
+
+players = ["son", "park"]
+
+let human = persons.boy
+
+persons = "persons"
+
+human = null
